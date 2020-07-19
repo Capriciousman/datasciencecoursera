992c4f8566efb4fb515b7193adc13f5dc64cd523
Ajay Kumar <44158088+Capriciousman@users.noreply.github.com>
2020-07-19 12:04:58 +0530

    Initial commit, test.r in R 4.0.0

---
+++ test.r
@@ -0,0 +1,10 @@
+---
+title: "test.R"
+author: "AJAY KUMAR"
+date: "7/19/2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+print("This file is created by R studio and commited changes in github")
+```
